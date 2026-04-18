df54a96aec4f2a5d16b0a87cd639153c045e8e66
ttokennxyz <84786285+ttokennxyz@users.noreply.github.com>
2026-04-18 20:27:46 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 local repo = "https://raw.githubusercontent.com/ttokennxyz/vaultcc/refs/heads/main/"
 if game.PlaceId == 87444640442831 or game.PlaceId == 121583187398542 then
-  loadstring(game:HttpGet(repo .. "untitledshooter2.luau"))
+  loadstring(game:HttpGet(repo .. "untitledshooter2.lua"))
 end
