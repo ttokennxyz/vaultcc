46c4a11804e21f5fb2f23cafb3ef463953a9fd93
ttokennxyz <84786285+ttokennxyz@users.noreply.github.com>
2026-09-01 10:28:37 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 13687899540 or game.PlaceId == 92518636938049 or game.PlaceId == 121650045752508 then -- cold war
   loadstring(game:HttpGet('https://api.luaauth.com/loader/N1Qr2e6217CxdoCGgc0Edi065mYjshAm'))()
+elseif game.PlaceId == 2916899287 or game.PlaceId == 10938546013 then -- brm5
+  loadstring(game:HttpGet('https://api.luaauth.com/loader/Rw7gdR6csUPEWkBXEiCNUYltflGjKwVh'))()
 end
