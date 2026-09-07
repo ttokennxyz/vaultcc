7ef0ce03eb705733802d5cf119c03a0c454177bf
ttokennxyz <84786285+ttokennxyz@users.noreply.github.com>
2026-09-07 21:17:11 +0200

    Change load from 'op1' to 'op1_loader'

--- loader.lua
+++ loader.lua
@@ -7,5 +7,5 @@ if game.PlaceId == 2916899287 or game.PlaceId == 10938546013 then
 elseif game.PlaceId == 13687899540 or game.PlaceId == 92518636938049 or game.PlaceId == 121650045752508 then
   load("coldwar")
 elseif game.PlaceId == 72920620366355 then
-  --load("op1")
+  load("op1_loader")
 end
